2ffa57ef0a7648c5605feb9b4f26357dbdfbd68e
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2020-12-22 16:53:45 -0600

    12/22/2020

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,9 +1,9 @@
 package org.launchcode.techjobs_oo.Tests;
 import org.junit.Test;
 import org.junit.Before;
-import org.launchcode.techjobs_oo.Job;
-import static org.junit.Assert.assertFalse;
+import org.launchcode.techjobs_oo.*;
 
+import static org.junit.Assert.*;
 
 
 public class JobTest {
@@ -22,4 +22,30 @@ public class JobTest {
 
     }
 
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob3 instanceof Job);
+        assertTrue(testJob3.getEmployer() instanceof Employer);
+        assertTrue(testJob3.getLocation() instanceof Location);
+        assertTrue(testJob3.getPositionType() instanceof PositionType);
+        assertTrue(testJob3.getCoreCompetency() instanceof CoreCompetency);
+
+
+        assertTrue("ACME" == testJob3.getEmployer().getValue());
+        assertTrue("Desert" == testJob3.getLocation().getValue());
+        assertTrue("Quality control" == testJob3.getPositionType().getValue());
+        assertTrue("Persistence" == testJob3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(testJob4.equals(testJob5));
+
+    }
 }
+
+
